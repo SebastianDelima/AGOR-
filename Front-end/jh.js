2926ba52364cf9e841dcfe051220aad79b84907e
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-06-19 16:18:35 -0400

    d

--- Front-end/jh.js
+++ Front-end/jh.js
@@ -1,10 +1,23 @@
 
 const isPalindrome = (string, counter1, counter2, center) => {
-    let palindrome= a.split("").reverse().join('')
-   }
-   
-   const a = "mom"
-   
-   
-   isPalindrome(a, 0, a.length -1, false);
-   
+    let palindrome;
+  
+      if(counter1 + counter2 == 0){
+        console.log("hello")
+         finished = true;
+      } else if(string[counter1] == string[counter2]){
+         palindrome = true
+         isPalindrome(string, counter1 + 1, counter2 - 1)
+      } else if(string[counter1] != string[counter2]){
+        console.log(counter1 + 1)
+        console.log(counter2 - 1)
+          return false
+    } 
+   return palindrome
+  }
+  
+  const a = "monm"
+  
+  
+  isPalindrome(a, 0, a.length -1, false);
+  
