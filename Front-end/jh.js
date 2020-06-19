9fb5ece1913e53666ccec48d276bfc9c029d3eff
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-06-19 16:17:52 -0400

    d

---
+++ Front-end/jh.js
@@ -0,0 +1,10 @@
+
+const isPalindrome = (string, counter1, counter2, center) => {
+    let palindrome= a.split("").reverse().join('')
+   }
+   
+   const a = "mom"
+   
+   
+   isPalindrome(a, 0, a.length -1, false);
+   
